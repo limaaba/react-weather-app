985f3a02ba66872f3edca107259a343197d25ea4
limaaba <limaabayonli170@gmail.com>
2026-04-29 14:09:35 +0000

    1

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '452f06de24msh5cd02a830ea972dp1023d6jsn0b597e21b83f',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
